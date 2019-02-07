1d12ca27fac16c4aa031ccd82e3b9a2c9aabda80
qtgeo1248 <gzhou00@stuy.edu>
2019-02-07 09:56:44 -0500

    set every value to 0

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,6 +3,11 @@ public class QueenBoard {
     private int[][] board;
 
     public QueenBoard(int size) {
-        board = int[size][size];
+        board = new int[size][size];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = 0;
+            }
+        }
     }
 }
