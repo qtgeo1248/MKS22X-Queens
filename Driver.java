597c70937bf7ed1f64ebd3acad8a70db161addf4
qtgeo1248 <gzhou00@stuy.edu>
2019-02-07 16:15:39 -0500

    debugging

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ public class Driver {
     public static void main(String[] args) {
         QueenBoard test = new QueenBoard(4);
         System.out.println(test + "\n");
-        test.addQueen(1, 0);
+        System.out.println(test.addQueen(1, 0));
         System.out.println(test);
     }
 }
