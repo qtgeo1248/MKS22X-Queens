de0710e01cde92dd6346c6bafc3e70113284af23
qtgeo1248 <gzhou00@stuy.edu>
2019-02-07 09:58:59 -0500

    set everything up for toSTring

--- QueenBoard.java
+++ QueenBoard.java
@@ -6,8 +6,18 @@ public class QueenBoard {
         board = new int[size][size];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = 0;
+                board[i][j] = 0; //0 represents a non-attacked space
             }
         }
+    }
+
+    public String toString() {
+        String output = "";
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+
+            }
+        }
+        return output;
     }
 }
