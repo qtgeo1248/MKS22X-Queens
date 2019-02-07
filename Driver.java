06ae08aaab2d40b046b4cad589037bc9234d0f42
qtgeo1248 <gzhou00@stuy.edu>
2019-02-07 16:13:20 -0500

    fixed boolean again

--- Driver.java
+++ Driver.java
@@ -1,7 +1,7 @@
 public class Driver {
     public static void main(String[] args) {
         QueenBoard test = new QueenBoard(4);
-        System.out.println(test);
+        System.out.println(test + "\n");
         test.addQueen(1, 0);
         System.out.println(test);
     }
