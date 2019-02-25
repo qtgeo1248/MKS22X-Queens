267d4e22563af40333aaa20b809c1e5917e248df
qtgeo1248 <gzhou00@stuy.edu>
2019-02-25 11:33:41 -0500

    fixed spacing

--- KDriver.java
+++ KDriver.java
@@ -1,18 +1,17 @@
 public class KDriver {
-    public static void runTest(int i){
+    public static void runTest(int i) {
         QueenBoard b;
-        int[]tests =   {1,2,3,4,5,8};
-        int[]answers = {1,0,0,2,10,92};
-        if(i >= 0 && i < tests.length ){
+        int[] tests = {1, 2, 3, 4, 5, 8};
+        int[] answers = {1, 0, 0, 2, 10, 92};
+        if (i >= 0 && i < tests.length) {
             int size = tests[i];
             int correct = answers[i];
             b = new QueenBoard(size);
-            int ans  = b.countSolutions();
-
-            if(correct==ans){
-                System.out.println("PASS board size: "+tests[i]+" "+ans);
-            }else{
-                System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+            int ans = b.countSolutions();
+            if (correct == ans) {
+                System.out.println("PASS board size: " + tests[i] + " " + ans);
+            } else {
+                System.out.println("FAIL board size: " + tests[i] + " " + ans + " vs " + correct);
             }
         }
     }
