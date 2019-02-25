74586f989e95b27f39e9810abda96a91bc279407
qtgeo1248 <gzhou00@stuy.edu>
2019-02-25 11:34:21 -0500

    made a main

--- KDriver.java
+++ KDriver.java
@@ -15,4 +15,9 @@ public class KDriver {
             }
         }
     }
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            runTest(i);
+        }
+    }
 }
